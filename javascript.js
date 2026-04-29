0d5b51768129f5982f405c13da3e39983752cf39
htsteelp1 <htsteelp1@gmail.com>
2026-04-29 14:36:32 -0400

    Added a deck generator

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
+let deck = []
+function deckGenerator() {
+    let suitList = "CHDS"
+    let numList = "A23456789TJQK"
+    for (let z = 0; z < 6; z++) {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 13; j++) {
+                deck.push(numList.at(j)+suitList.at(i));
+            }
+        }
+    }
+    deck.sort(() => Math.random() - 0.5); // Shuffles the deck
+}
 let balanceRef = {
     get value() {
         return localStorage.getItem("balance");
@@ -9,11 +22,16 @@ let balanceRef = {
 let balanceDis = document.querySelector("#balance");
 
 function initialLoad() {
-    balanceRef.value = balanceRef.vaulue>0 ? balanceRef.value : 100
-    localStorage.setItem("balance", balance);
+    balanceRef.value = balanceRef.value>0 ? balanceRef.value : 100
     displayBalance();
 }
+
+function dealCard() {
+
+}
+
 function displayBalance() {
     balanceDis.innerText = balanceRef.value;
 }
+
 initialLoad();
